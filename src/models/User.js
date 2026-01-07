b4576f5fcfcfd4f929ed2a75e1e9fa17763f8907
janchundiaws <junior_soza@hotmail.es>
2026-01-07 16:24:01 -0500

    Nuevo campo del token y el dispositivo que crea la cuenta

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,8 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     displayName: { type: String },
     pushTokens: [{ type: String }],
+    sessionToken: { type: String },
+    deviceId: { type: String },
     role: { type: String, enum: ["user", "admin"], default: "user" }, // <-- campo de rol
   },
   { timestamps: true }
--- src/utils/generateId.js
+++ src/utils/generateId.js
@@ -2,3 +2,7 @@ export function generateUserId() {
   // Genera un número aleatorio de 9 dígitos
   return Math.floor(100000000 + Math.random() * 900000000).toString();
 }
+export function generateDeviceId() {
+  // Genera un número aleatorio de 9 dígitos
+  return Math.floor(100000000 + Math.random() * 900000000).toString();
+}
